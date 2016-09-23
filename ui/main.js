5acfc70c7563524adbd48422ca21cc0ed8e6d426
abhishek63 <kumargolu63@gmail.com>
2016-09-23 20:12:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 
 // change the content of the webpage 
 var x = document.getElementById('text');
-x.innerHTML = "This is the my new webpage";
+x.innerHTML = 'This is the my new webpage';
 
