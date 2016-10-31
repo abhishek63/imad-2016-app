045f4e5de64a65b175523d85bafcab278458fdf3
abhishek63 <kumargolu63@gmail.com>
2016-11-01 02:17:13 +0530

    add homepage

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,3 +1,10 @@
 
 //create AngularJS function angular.module
 var app = angular.module('ImadBlog', ['ngRoute']);
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Homepage of the blog 
+    .when("/", {templateUrl: "include/home.html", controller: "PageCtrl"})
+    
+}]);
+
