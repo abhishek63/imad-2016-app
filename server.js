d4fe6792d2d447ddc0b1fb05a7ba125d757e55e8
Abhishek Kumar <kumargolu63@gmail.com>
2016-11-18 14:22:13 +0530

    Update server.js
    all devlopement done in local environment because imad server is too down

--- server.js
+++ server.js
@@ -1,168 +1,204 @@
+
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+
+var crypto = require('crypto');
+
+//database library
+
+
+
+
 
 var app = express();
 app.use(morgan('combined'));
 
 
 
+		var articles ={
+
+				'article-one' : {
+
+					title :'article-one | Abhishek',
+					heading:'Artile-one',
+					date:'15th september',
+					content:
+							`<p>
+								India clinched a memorable victory in their 900th ODI match as they beat New Zealand in the 1st ODI of 5-match ODI series at Dharamsala. After winning the toss, India decided to bowl first. Except for Tom Latham, all New Zealand top order batsmen failed against Indian bowling. With the help of some brave lower-order partnerships, New Zealand managed to score 190 in 43.5 overs (190/10).
+
+							</p>`
+
+				},
+
+				'article-two' : {
+
+					title :'article-two | Abhishek',
+					heading:'Artile-2',
+					date:'15th september',
+					content:
+							`<p>
+								India clinched a memorable victory in their 900th ODI match as they beat New Zealand in the 1st ODI of 5-match ODI series at Dharamsala. After winning the toss, India decided to bowl first. Except for Tom Latham, all New Zealand top order batsmen failed against Indian bowling. With the help of some brave lower-order partnerships, New Zealand managed to score 190 in 43.5 overs (190/10).
+
+							</p>`
+
+				},
+
+				'article-three' : {
+
+					title :'article-three | Abhishek',
+					heading:'Artile-3',
+					date:'15th september',
+					content:
+							`<p>
+								India clinched a memorable victory in their 900th ODI match as they beat New Zealand in the 1st ODI of 5-match ODI series at Dharamsala. After winning the toss, India decided to bowl first. Except for Tom Latham, all New Zealand top order batsmen failed against Indian bowling. With the help of some brave lower-order partnerships, New Zealand managed to score 190 in 43.5 overs (190/10).
+
+							</p>`
+
+				}
+
+			};
 
 
-var articles={
 
-'article-one':{
-	title:'Article-one | Hello world',
-	heading:'Article-one',
-	content:`<p>
-				I am in haldia Institute of technology. I am in haldia Institute of technology.
-				I am in haldia Institute of technology.
-				I am in haldia Institute of technology.I am in haldia Institute of technology.
+function createHtmlTemplate(data){
 
-			</p>
-
-			<p>
-				I am in haldia Institute of technology. I am in haldia Institute of technology.
-				I am in haldia Institute of technology.
-				I am in haldia Institute of technology.I am in haldia Institute of technology.
-
-			</p>
-			<p>
-				I am in haldia Institute of technology. I am in haldia Institute of technology.
-				I am in haldia Institute of technology.
-				I am in haldia Institute of technology.I am in haldia Institute of technology.
-
-			</p>`
-},
-'article-two':{
-	title:'Article-two | Hello world',
-	heading:'Article-one',
-	content:`<p>
-				I am in haldia Institute of technology. I am in haldia Institute of technology.
-				I am in haldia Institute of technology.
-				I am in haldia Institute of technology.I am in haldia Institute of technology.
+		var title = data.title;
+		var heading = data.heading;
+		var date =  data.date;
+		var content = data.content;
 
 	
 
-			</p>`
-},
+	var htmlTemplate = `
 
-'article-three':{
-	title:'Article-three | Hello world',
-	heading:'Article-one',
-	content:`<p>
-				I am in haldia Institute of technology. I am in haldia Institute of technology.
-				I am in haldia Institute of technology.
-				I am in haldia Institute of technology.I am in haldia Institute of technology.
+					<html>
+			<head>
+			<title>${title}</title>
+			</head>
 
-			</p>`
+			<body>
 
-}
-};
+				<div id="container">
 
+					<h1>
 
-function createTemplate(data){
+						${heading} 
 
-	title = data.title;
-	content= data.content;
-	heading = data.heading;
+					</h1>
 
+					<h2>
+						Created by --- Abhishek  ${date}
+					</h2>
+						
+						${content}
+				</div>
+			</body>
 
-
-	var htmlTemplate=`
-		<!DOCTYPE html>
-	<html>
-	<head>
-		<title>
-			${title}
-		</title>
-		<meta name="viewport" content="width=device-width,intial-scale=1"/>
-
-		<style>
-		.container{
-			max-width: 800px;
-			font-family: sans-serif;
-			padding-left: 50px;
-			padding-right: 50px;
-			padding-top: 50px;
-			background: burlywood;
-			margin:0 auto;
-		}
+			</html>
+	`;
 
 
-		</style>
-	</head>
-	<body>
-
-		<div class="container">
-
-			<div>
-				<a href=".\">Home</a>
-			</div>
-			<hr>
-
-			<div>
-				<h1>
-					${heading}
-				</h1>
-			</div>
-
-			<div>
-				${content}
-
-				</p>
-			</div>
-
-
-
-		</div>
-
-	</body>
-	</html>
-
-	` ;
 	return htmlTemplate;
-
 }
 
 
 
-var counter =0;
-app.get('/counter', function (req, res) {
+function hash(input,salt){
 
-	counter=counter+1;
-  res.send(counter.toString());
+	var hashed =  crypto.pbkdf2Sync(input, salt, 1, 512, 'sha512');
+
+	return hashed.toString('hex');
+
+}
+
+
+app.get('/hash/:input', function (req, res) {
+
+	var hashedString = hash(req.params.input,'this-is-a-random-string');
+
+	res.send(hashedString);
+
+  
+
 
 });
-
-
-
-
-app.get('/:articleName',function(req,res){
-	
-
-	var articleName = req.params.articleName;
-
-	res.send(createTemplate(articles[articleName]));
-});
-
 
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/js/main.js',function(req,res){
+   res.sendFile(path.join(__dirname,'ui/js','main.js')); 
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+
+app.get('/ui/css/main.css',function(req,res){
+   res.sendFile(path.join(__dirname,'ui/css','main.css')); 
 });
 
-app.get('/ui/main.js',function(req,res){
-   res.sendFile(path.join(__dirname,'ui','main.js')); 
+
+app.get('/ui/css/about.css',function(req,res){
+   res.sendFile(path.join(__dirname,'ui/css','about.css')); 
 });
+
+app.get('/ui/css/logincss.css',function(req,res){
+   res.sendFile(path.join(__dirname,'ui/css','logincss.css')); 
+});
+
+
+app.get('/ui/include/login.html',function(req,res){
+   res.sendFile(path.join(__dirname,'ui/include','login.html')); 
+});
+
+app.get('/ui/include/register.html',function(req,res){
+   res.sendFile(path.join(__dirname,'ui/include','register.html')); 
+});
+
+app.get('/ui/img.jpg',function(req,res){
+   res.sendFile(path.join(__dirname,'ui','img.jpg')); 
+});
+
+
+app.get('/ui/include/about.html',function(req,res){
+   res.sendFile(path.join(__dirname,'ui/include','about.html')); 
+});
+
+app.get('/ui/include/login.html',function(req,res){
+   res.sendFile(path.join(__dirname,'ui/include','login.html')); 
+});
+
+app.get('/ui/include/home.html',function(req,res){
+   res.sendFile(path.join(__dirname,'ui/include','home.html')); 
+});
+
+
+
+app.get('/ui/header.html',function(req,res){
+   res.sendFile(path.join(__dirname,'ui','header.html')); 
+});
+
+
+
+
+app.get('/ui/footer.html',function(req,res){
+   res.sendFile(path.join(__dirname,'ui','footer.html')); 
+});
+
+
+app.get('/:articleName',function(req,res){
+
+
+	var articleName = req.params.articleName;
+
+	console.log("articleName");
+
+
+	res.send(createHtmlTemplate(articles[articleName]));
+
+});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
