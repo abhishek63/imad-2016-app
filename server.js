e5b64f9637a114a6f09d3f9a79c1ff07f14ba27c
Abhishek Kumar <kumargolu63@gmail.com>
2016-11-18 21:36:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get('/test-db',function(req,res){
        res.status(500).send(err.toString());
        }
        else{
-           res.send(JSON.stringify(result));
+           res.send(JSON.stringify(result.rows));
        }
    });
     
