00c691d37905049c532f74200e7b40759327c6c2
Abhishek Kumar <kumargolu63@gmail.com>
2016-11-19 11:51:12 +0530

    [imad-console] Updates ui/js/main.js

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,14 +1,6 @@
 //create AngularJS function angular.module
-var app = angular.module('ImadBlog', ['ngRoute']);
-app.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    // Homepage of the blog 
-    .when("/", {templateUrl: "/ui/include/home.html", controller: "PageCtrl"})
-    .when("/about", {templateUrl: "/ui/include/about.html", controller: "PageCtrl"})
-    .when("/login", {templateUrl: "/ui/include/login.html", controller: "PageCtrl"})
-    .when("/home", {templateUrl: "/ui/include/home.html", controller: "PageCtrl"})
-    .when("/register", {templateUrl: "/ui/include/register.html", controller: "PageCtrl"})
-}]);
+var app = angular.module('ImadBlog', []);
+
 //create AngularJS function angular.module
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   console.log("Page Controller reporting for duty.");
